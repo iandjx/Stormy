a58b1bce4c6b48203d4d7071b6437d45579212a4
iandjx <ian.dejesus.md@gmail.com>
2016-05-14 06:56:04 +0900

    Added a view per daily item forecast

--- app/src/main/java/com/example/android/stormy/DailyForecastActivity.java
+++ app/src/main/java/com/example/android/stormy/DailyForecastActivity.java
@@ -3,6 +3,7 @@ package com.example.android.stormy;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class DailyForecastActivity extends ListActivity {
 
@@ -12,5 +13,8 @@ public class DailyForecastActivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, daysOfTheWeek );
+        setListAdapter(adapter);
     }
 }
